5717a97ba4d1d0d0111c75530e19cc7e18acb140
Konstantin <37781449+Kosankuz@users.noreply.github.com>
2018-08-11 01:32:31 +0100

    Add files via upload

---
+++ js/button_disable_enable.js
@@ -0,0 +1,16 @@
+
+var cap = 100;
+var reached = 150;
+var buy_myetherwallet = document.querySelector('#buy_myetherwallet');
+
+setInterval (function() {
+
+
+
+if (reached >= cap) {
+buy_myetherwallet.setAttribute('disabled','');
+} else {
+
+}
+
+},10);
