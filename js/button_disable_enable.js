6d3825f2beaef40f4b422e1910a419bb6d600dca
devcontract <38960225+devcontract@users.noreply.github.com>
2018-08-11 21:47:49 +0300

    Add files via upload

--- js/button_disable_enable.js
+++ js/button_disable_enable.js
@@ -1,101 +1,32 @@
 
-var cap = 100;
-var reached = 50;
-
-
-/////////////////////////////// purchase_details_ block buttons
+var trig;
 setInterval (function() {
+   if (trig == 1) {
+     allowed("buy_myetherwallet");
+     //allowed("buy_metamask");
+     allowed("withdraw_myetherwallet");
+     //llowed("withdraw_metamask");
+     allowed("refund_myetherwallet");
+    // allowed("refund_metamask");
+     trig=0;
+   } else {
+     non_allowed("buy_myetherwallet");
+    // non_allowed("buy_mmetamask");
+     non_allowed("withdraw_myetherwallet");
+   //  non_llowed("withdraw_metamask");
+     non_allowed("refund_myetherwallet");
+   //  non_allowed("refund_metamask");
+     trig=1;
+   }
 
-if (reached >= cap) {
+},1000);
 
-  var css_myetherwallet = '#buy_myetherwallet:hover{cursor: not-allowed}';
-  var css_metamask ='#buy_metamask:hover{cursor:not-allowed}';
-
-  var style = document.createElement('style');
-
-  style.appendChild(document.createTextNode(css_myetherwallet));
-  document.getElementsByTagName('head')[0].appendChild(style);
-  style.appendChild(document.createTextNode(css_metamask));
-  document.getElementsByTagName('head')[0].appendChild(style);
-
-document.getElementById('buy_myetherwallet').style.backgroundColor = "#cccccc";
-document.getElementById('buy_myetherwallet').style.color ="#737277";
-document.getElementById('buy_myetherwallet').setAttribute('onclick' ,'return false');
-
-document.getElementById('buy_metamask').style.backgroundColor = "#cccccc";
-document.getElementById('buy_metamask').style.color ="#737277";
-document.getElementById('buy_metamask').setAttribute('onclick' ,'return false');
-
-} else {
-
+function allowed(id){
+ document.getElementById(id).className = "allowed_button";
+ document.getElementById(id).setAttribute('onclick' ,'return true');
 }
 
-},10);
-/////////////////////////////// purchase_details_ block buttons
-
-/////////////////////////////// withdrawal  block buttons
-setInterval (function() {
-
-if (reached >= cap) {
-
-  var css_myetherwallet = '#withdraw_myetherwallet:hover{cursor: not-allowed}';
-  var css_metamask ='#withdraw_metamask:hover{cursor:not-allowed}';
-
-  var style = document.createElement('style');
-
-  style.appendChild(document.createTextNode(css_myetherwallet));
-  document.getElementsByTagName('head')[0].appendChild(style);
-  style.appendChild(document.createTextNode(css_metamask));
-  document.getElementsByTagName('head')[0].appendChild(style);
-
-document.getElementById('withdraw_myetherwallet').style.backgroundColor = "#cccccc";
-document.getElementById('withdraw_myetherwallet').style.color ="#737277";
-document.getElementById('withdraw_myetherwallet').setAttribute('onclick' ,'return false');
-
-document.getElementById('withdraw_metamask').style.backgroundColor = "#cccccc";
-document.getElementById('withdraw_metamask').style.color ="#737277";
-document.getElementById('withdraw_metamask').setAttribute('onclick' ,'return false');
-
-} else {
-
+function non_allowed(id){
+document.getElementById(id).className = "non_allowed_button";
+document.getElementById(id).setAttribute('onclick' ,'return false');
 }
-
-},10);
-/////////////////////////////// withdrawal  block buttons
-
-
-/////////////////////////////// withdrawal  block buttons
-setInterval (function() {
-
-if (reached >= cap) {
-
-  var css_myetherwallet = '#refund_myetherwallet:hover{cursor: not-allowed}';
-  var css_metamask ='#refund_metamask:hover{cursor:not-allowed}';
-
-  var style = document.createElement('style');
-
-  style.appendChild(document.createTextNode(css_myetherwallet));
-  document.getElementsByTagName('head')[0].appendChild(style);
-  style.appendChild(document.createTextNode(css_metamask));
-  document.getElementsByTagName('head')[0].appendChild(style);
-
-document.getElementById('refund_myetherwallet').style.backgroundColor = "#cccccc";
-document.getElementById('refund_myetherwallet').style.color ="#737277";
-document.getElementById('refund_myetherwallet').setAttribute('onclick' ,'return false');
-
-document.getElementById('refund_metamask').style.backgroundColor = "#cccccc";
-document.getElementById('refund_metamask').style.color ="#737277";
-document.getElementById('refund_metamask').setAttribute('onclick' ,'return false');
-
-} else {
-
-}
-
-},10);
-/////////////////////////////// withdrawal  block buttons
-
-
-
-setInterval (function(){
-  reached = 101;
-},10000);
