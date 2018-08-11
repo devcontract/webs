d71aa44895dabb164a00bd4e7146c7ff82627da6
devcontract <38960225+devcontract@users.noreply.github.com>
2018-08-11 19:17:40 +0300

    Add files via upload

--- log.js
+++ log.js
@@ -1,61 +1,64 @@
 
-
-var indexNum = 1;
-var i = 0;
 var txHref = "https://etherscan.io";
 
 function log(){
    showLog();
-  // line();
+ 
 
 }
 
 function  showLog(){
-var base = document.getElementById('stringbase');
+ for (var i = 1; i <= 4; i++) {
      createBase();
-     while (base.hasChildNodes()) {
-        base.removeChild(base.firstChild);
-    }
-     createDiv(rand());
-     createDiv(new Date().toDateString("yyyy/MM/dd"));
-     createButton("refund", "green");
-     createDiv("TES");
-     createDiv(rand());
-     createDiv(rand());
-     createDiv(rand());
-     createButton("pending", "red");
+     createDiv(i, 20);
+     createDiv("09/07/2018, 06:10:46", 150);
+     createDiv("0x2525821be8215769dd61fc7d25b83fefede62efbcf4a7edac271a87c44e03003", 420);
+     createDiv("2602384", 50);
+     createDiv("2.123456789123457", 150);
+     createButton(txHref, "Fail", "red", 150);
+     
+     createReturn();
+ }
 }
 
 function rand(){
    return Math.floor((Math.random() * 8995) + 10);
 }
-function createButton(data, color){
+
+function createButton(txHref, data, color, elemWidth){
    var a = document.createElement("a");
    a.innerHTML = data +  "<br><br>";
    a.style.color = color;
    a.style.height = "100%";
-   a.id = indexNum;
    a.style.cursor = "pointer";
    a.style.backgroundColor = color;
    a.style.border = 'none';
+   a.style.textDecoration = "none";
    a.style.color = "white";
    a.style.height = "30px";
    a.style.paddingTop = "4px";
-   a.style.textDecoration = "none";
    a.style.textShadow = '2px 2px black';
    a.setAttribute('class', 'grid2');
+   a.style.width = elemWidth + "px";
    a.setAttribute('href', txHref);
    a.setAttribute('target', '_blank');
    document.getElementById('stringbase').appendChild(a);
- }
+}
 
-function createDiv(data){
+function createDiv(data, elemWidth){
    var div = document.createElement("div");
    div.innerHTML = data;
    div.style.color = 'black';
-   div.setAttribute('class', 'grid2');
+   div.setAttribute('class', "grid2");
+   div.style.width=elemWidth + "px";
    document.getElementById('stringbase').appendChild(div);
 }
+
+function createReturn(){
+   var newElem = document.createElement("BR");
+   document.getElementById('stringbase').appendChild(newElem);
+}
+
 function createBase(){
    var div = document.createElement("div");
    div.style.backgroundColor = "none";
@@ -63,16 +66,4 @@ function createBase(){
    div.id = 'stringbase';
    div.setAttribute('class', 'grid2');
    document.getElementById('string').appendChild(div);
-
-}
-
-
-
-function line(){
-   i = i + 40;
-   var pixel;
-    for (var index = 1; index <= 7; index++) {
-       pixel = document.querySelector("#img" + index);
-       pixel.setAttribute("height", i);
-    }
 }
