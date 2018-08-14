cc92119203859c662fef11bda62d908c3a880f46
devcontract <38960225+devcontract@users.noreply.github.com>
2018-08-15 01:46:02 +0300

    updated work with button

--- js/button_disable_enable.js
+++ js/button_disable_enable.js
@@ -2,41 +2,31 @@
 var trig;
 setInterval (function() {
    if (trig == 1) {
-     allowed_myetherwallet("buy_myetherwallet");
-     allowed_mmetamask("buy_metamask");
-     allowed_myetherwallet("withdraw_myetherwallet");
-     allowed_mmetamask("withdraw_metamask");
-     allowed_myetherwallet("refund_myetherwallet");
-     allowed_mmetamask("refund_metamask");
+     allowed_button("buy_myetherwallet");
+     allowed_button("buy_metamask");
+     allowed_button("withdraw_myetherwallet");
+     allowed_button("withdraw_metamask");
+     allowed_button("refund_myetherwallet");
+     allowed_button("refund_metamask");
      trig=0;
    } else {
-     non_allowed_myetherwallet("buy_myetherwallet");
-     non_allowed_mmetamask("buy_metamask");
-     non_allowed_myetherwallet("withdraw_myetherwallet");
-     non_allowed_mmetamask("withdraw_metamask");
-     non_allowed_myetherwallet("refund_myetherwallet");
-     non_allowed_mmetamask("refund_metamask");
+     non_allowed_button("buy_myetherwallet");
+     non_allowed_button("buy_metamask");
+     non_allowed_button("withdraw_myetherwallet");
+     non_allowed_button("withdraw_metamask");
+     non_allowed_button("refund_myetherwallet");
+     non_allowed_button("refund_metamask");
      trig=1;
    }
 
 },2000);
 
-function allowed_myetherwallet(id){
-   document.getElementById(id).className = "allowed_myetherwallet";
+function allowed_button(id){
+   document.getElementById(id).className = "allowed_button";
    document.getElementById(id).setAttribute('onclick' ,'return true');
 }
 
-function non_allowed_myetherwallet(id){
-   document.getElementById(id).className = "non_allowed_myetherwallet";
+function non_allowed_button(id){
+   document.getElementById(id).className = "non_allowed_button";
    document.getElementById(id).setAttribute('onclick' ,'return false');
-}
-
-function allowed_mmetamask(id){
-   document.getElementById(id).className = "allowed_metamask";
-   document.getElementById(id).setAttribute('onclick' ,'return true');
-}
-
-function non_allowed_mmetamask(id){
-  document.getElementById(id).className = "non_allowed_metamask";
-  document.getElementById(id).setAttribute('onclick' ,'return false');
 }
