a9ab65da8d4c0c400dc46fce0c551e1edd3a8f89
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-22 22:49:56 +0100

    logic

--- js/img_bar.js
+++ js/img_bar.js
@@ -1,13 +1,7 @@
 $(document).ready(function(){
-
 var i = 0;
 var timerId = setInterval(function() { if (i >= 5) {i = 0};i++;
 $("#bar_state" + i).show();
 $("#bar_state" + (i - 1) ).hide();
-
 },1000);
-$("button").click(function(){
-  $("#bar_state1").toggle()
-});
-
 });
