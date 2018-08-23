f5ba5e5dc905f1ac35acbc87e0b320585ba5014c
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-24 00:30:20 +0100

    beautify

--- js/parsingJson_module.js
+++ js/parsingJson_module.js
@@ -1,14 +1,18 @@
-export function getEthPice(){ setInterval(function(){
-               $.getJSON('https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken',
-               function(jd) { $("#ethusd").text($("#raised").text() * jd.result["ethusd"] );});
+export function getEthPice() {
+  setInterval(function() {
+    $.getJSON('https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken', // getting Json from APi
+      function(jd) {
+        $("#ethusd").text($("#raised").text() * jd.result["ethusd"]);
+      }); // Displaying Jason object that we want
+  }, 100);
+}; // Interval in MiliSeconds
 
-
-},100);};
-
-export function getEurToUsd(){
-  $.getJSON('http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y', function(eur){
-    var eur = ($("#ethusd").text() / eur.EUR_USD["val"]).toFixed(2);
-    var number = Number(eur);
-$("#inEuro").text( number );
-})
+export function getEurToUsd() {
+  setInterval(function() {
+    $.getJSON('http://free.currencyconverterapi.com/api/v5/convert?q=EUR_USD&compact=y', function(eur) {
+      var eur = ($("#ethusd").text() / eur.EUR_USD["val"]).toFixed(2);
+      var number = Number(eur);
+      $("#inEuro").text(number);
+    })
+  }, 100);
 };
