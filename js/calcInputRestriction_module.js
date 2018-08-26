81fce499b3df3b200a4c51f2f4eb060ff7551dc9
kosankuz <konstantinkuzmenko@icloud.com>
2018-08-26 21:55:06 +0100

    .

--- js/calcInputRestriction_module.js
+++ js/calcInputRestriction_module.js
@@ -1,7 +1,6 @@
-var str = 'mississippi';
-
 export function find_occurences(str, char_to_count){
     return str.split(char_to_count).length - 1;
-}
-
-find_occurences(str, 'i') //outputs 4
+};
+setI
+var inputvalue $("#input_value").val();
+find_occurences( r , ".");
--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@
 import {timerId} from './progresbar_module.js'
 import {showFiatPrice} from './fiatPrice_module.js'
 import {showTransaction} from './transaction_module.js'
-import {find_occurences} from './calcInputRestriction_module.js'
+//import {find_occurences} from './calcInputRestriction_module.js'
 $(document).ready(function(){
 
 //$("#buy_metamask").click(function(){buyToken()});
 showFiatPrice();
 timerId();
 showTransaction();
+
 
 
 
@@ -24,7 +25,7 @@ document.getElementById('raised').innerHTML = "1";
 document.getElementById('soft').innerHTML = "9876";
 document.getElementById('hard').innerHTML = "44444";
 document.getElementById('tokensold').innerHTML = "43433434";
-document.getElementById('value_token').innerHTML = "6876876";
+document.getElementById('value_token').innerHTML = "123321123";
 document.getElementById('paymentaddr').value = "0x66a945a11483e05bedb37474ca29ad6e8656c4f4";
 document.getElementById('gLimitBay').innerHTML = "2200";
 document.getElementById('gprice').innerHTML = "4000";
